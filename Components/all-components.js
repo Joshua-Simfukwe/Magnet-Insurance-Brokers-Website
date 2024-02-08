428ce469c7fd0555068821e59c4694ca9d2eab0e
Joshua Simfukwe <joshuasimfukwe73@gmail.com>
2024-02-08 17:42:44 +0200

    Initial Commit

---
+++ Components/all-components.js
@@ -0,0 +1,174 @@
+class Header extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+         <div data-w-id="9ae6fa4b-1ebb-a8ad-88fa-127ad450080c" data-animation="default" data-collapse="medium"
+        data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="nav-section w-nav">
+        <div class="container nav">
+            <div class="navbar-inner"><a href="#" class="uui-navbar08_logo-link-2 w-nav-brand"><img
+                        src="../Assets/Logo Og.png"
+                        loading="lazy" sizes="(max-width: 479px) 162.71875px, 182.359375px"
+                        srcset="../Assets/Logo-p-500.png 500w, ../Assets/Logo-p-895.png 895w"
+                        alt="Untitled UI logotext" class="uui-logo_logotype-3" /></a>
+                <nav role="navigation" class="uui-navbar08_menu-2 w-nav-menu">
+                    <div class="uui-navbar08_menu-left-2"><a href="index.html"
+                            class="uui-navbar08_link-2 w-nav-link">Home</a><a href="about-us.html" aria-current="page"
+                            class="uui-navbar08_link-2 w-nav-link w--current">About</a>
+                        <div data-hover="true" data-delay="300" data-w-id="9ae6fa4b-1ebb-a8ad-88fa-127ad4500819"
+                            class="uui-navbar08_menu-dropdown-2 w-dropdown">
+                            <div class="uui-navbar08_dropdown-toggle-2 w-dropdown-toggle">
+                                <div class="uui-dropdown-icon-2 w-embed"><svg width="20" height="20" viewBox="0 0 20 20"
+                                        fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M5 7.5L10 12.5L15 7.5" stroke="CurrentColor" stroke-width="1.67"
+                                            stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg></div>
+                                <div class="text-block-2">Products</div>
+                            </div>
+                            <nav class="uui-navbar08_dropdown-list-2 w-dropdown-list">
+                                <div class="uui-navbar08_dropdown-content-2">
+                                    <div class="uui-navbar08_dropdown-link-list-2"><a href="motor-insurance.html"
+                                            class="uui-navbar08_dropdown-link-2 w-inline-block">
+                                            <div class="uui-navbar08_icon-wrapper-2"><img
+                                                    src="../Assets/Nav icons/Motor.svg"
+                                                    loading="lazy" alt="" /></div>
+                                            <div class="uui-navbar08_item-right-2">
+                                                <div class="uui-navbar08_item-heading-2">Motor Insurance</div>
+                                            </div>
+                                        </a><a href="agriculture-insurance.html"
+                                            class="uui-navbar08_dropdown-link-2 w-inline-block">
+                                            <div class="uui-navbar08_icon-wrapper-2"><img
+                                                    src="../Assets/Nav icons/Agri.svg"
+                                                    loading="lazy" alt="" /></div>
+                                            <div class="uui-navbar08_item-right-2">
+                                                <div class="uui-navbar08_item-heading-2">Agriculture Insurance</div>
+                                            </div>
+                                        </a><a href="fire-insurance.html"
+                                            class="uui-navbar08_dropdown-link-2 w-inline-block">
+                                            <div class="uui-navbar08_icon-wrapper-2"><img
+                                                    src="../Assets/Nav icons/fire.svg"
+                                                    loading="lazy" alt="" /></div>
+                                            <div class="uui-navbar08_item-right-2">
+                                                <div class="uui-navbar08_item-heading-2">Fire Insurance</div>
+                                            </div>
+                                        </a><a href="corporate-insurance.html"
+                                            class="uui-navbar08_dropdown-link-2 w-inline-block">
+                                            <div class="uui-navbar08_icon-wrapper-2"><img
+                                                    src="../Assets/Nav icons/corporate.svg"
+                                                    loading="lazy" alt="" /></div>
+                                            <div id="w-node-_9ae6fa4b-1ebb-a8ad-88fa-127ad4500836-d450080c"
+                                                class="uui-navbar08_item-right-2">
+                                                <div class="uui-navbar08_item-heading-2">Corporate Insurance</div>
+                                            </div>
+                                        </a></div>
+                                    <div class="uui-navbar08_dropdown-link-list-2"><a href="property-insurance.html"
+                                            class="uui-navbar08_dropdown-link-2 w-inline-block">
+                                            <div class="uui-navbar08_icon-wrapper-2"><img
+                                                    src="../Assets/Nav icons/property.svg"
+                                                    loading="lazy" alt="" class="image-11" /></div>
+                                            <div class="uui-navbar08_item-right-2">
+                                                <div class="uui-navbar08_item-heading-2">Property Insurance</div>
+                                            </div>
+                                        </a><a href="life-insurance.html"
+                                            class="uui-navbar08_dropdown-link-2 w-inline-block">
+                                            <div class="uui-navbar08_icon-wrapper-2"><img
+                                                    src="../Assets/Nav icons/life.svg"
+                                                    loading="lazy" alt="" /></div>
+                                            <div class="uui-navbar08_item-right-2">
+                                                <div class="uui-navbar08_item-heading-2">Life Insurance</div>
+                                            </div>
+                                        </a><a href="health-insurance.html"
+                                            class="uui-navbar08_dropdown-link-2 w-inline-block">
+                                            <div class="uui-navbar08_icon-wrapper-2"><img
+                                                    src="../Assets/Nav icons/health.svg"
+                                                    loading="lazy" alt="" /></div>
+                                            <div class="uui-navbar08_item-right-2">
+                                                <div class="uui-navbar08_item-heading-2">Health Insurance</div>
+                                            </div>
+                                        </a></div>
+                                </div>
+                            </nav>
+                        </div><a href="partners.html" class="uui-navbar08_link-2 w-nav-link">Partners</a>
+                    </div>
+                    <div class="uui-navbar08_menu-right-2">
+                        <div class="uui-navbar08_button-wrapper-2"><a id="Main-link-login" href="employees.html"
+                                target="_blank" class="button-primary outline w-button">Login</a><a
+                                id="Main-link-contact" href="contact.html" class="button-primary w-button">Contact us</a>
+                        </div>
+                    </div>
+                </nav>
+                <div class="uui-navbar08_menu-button-2 w-nav-button">
+                    <div class="menu-icon_component-2">
+                        <div class="menu-icon_line-top-2"></div>
+                        <div class="menu-icon_line-middle-2">
+                            <div class="menu-icon_line-middle-inner-2"></div>
+                        </div>
+                        <div class="menu-icon_line-bottom-2"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    }
+}
+
+//FOOTER COMPONENT
+class Footer extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `    
+            <section class="footer-subscribe">
+        <div class="container footer">
+            <div class="footer-form-two w-form">
+                <form action="mailto:joshuasimfukwe73@gmail.com" id="wf-form-Footer-Form-Two" name="wf-form-Footer-Form-Two" data-name="Footer Form Two"
+                    method="get" class="footer-form-container-two" data-wf-page-id="65b1ae7e4f0b1c2b962577a8"
+                    data-wf-element-id="372d589c-0b23-ac8e-edb8-437f49719395">
+                    <div class="heading-second bold">Subscribe for Updates </div>
+                    <div class="footer-form-block-two"><input class="footer-form-input w-input" maxlength="256"
+                            name="Footer-Email-Two-2" data-name="Footer Email Two 2" aria-label="Enter your email"
+                            placeholder="Enter your email" type="email" id="Footer-Email-Two-2" required="" /><input
+                            type="submit" data-wait="Please wait..." class="button-primary footer-form-button w-button"
+                            value="Subscribe now" /></div>
+                </form>
+                <div class="w-form-done">
+                    <div>Thank you! Your submission has been received!</div>
+                </div>
+                <div class="w-form-fail">
+                    <div>Oops! Something went wrong while submitting the form.</div>
+                </div>
+            </div>
+            <div class="w-layout-blockcontainer _48px-spacer w-container"></div>
+            <div class="footer-wrapper-three">
+                <div class="footer-block-three"><a id="About-footer-link" href="/about-us" aria-current="page"
+                        class="footer-link-three w--current">About us</a><a id="Partners-footer-link" href="/partners"
+                        class="footer-link-three">Partners</a><a id="contact-footer-link" href="/contact"
+                        class="footer-link-three">Contact us</a></div>
+                <div class="footer-social-block-three"><a id="facebook-icon"
+                        href="https://web.facebook.com/profile.php?id=61556235055796" target="_blank"
+                        class="footer-social-link-three w-inline-block"><img
+                            src="../Assets/footer icons/fb icon.svg"
+                            loading="lazy" alt="" class="image-8" /></a><a id="twitter-icon" href="#"
+                        class="footer-social-link-three w-inline-block"><img
+                            src="../Assets/footer icons/x.svg"
+                            loading="lazy" id="in" alt="" class="image-9" /></a><a href="#"
+                        class="footer-social-link-three w-inline-block"><img
+                            src="../Assets/footer icons/insta.svg"
+                            loading="lazy" alt="" class="image-10" /></a><a id="Linkedin-icon"
+                        href="https://www.linkedin.com/company/magnet-insurance-brokers-ltd-zambia/?viewAsMember=true"
+                        target="_blank" class="footer-social-link-three w-inline-block"><img
+                            src="../Assets/footer icons/in.svg"
+                            loading="lazy" alt="" class="image-7" /></a></div>
+            </div>
+            <div class="_48px-spacer"></div>
+            <div class="footer-bottom">
+                <div class="footer-copyright">© 2024 Magnet Insurance Brokers Ltd. All rights reserved</div>
+                <div class="footer-legal-block"><a href="https://www.linkedin.com/in/joshua-simfukwe-06489922a/"
+                        class="footer-legal-link">Designed and developed by Joshua Simfukwe</a></div>
+            </div>
+        </div>
+    </section>
+    `;
+    }
+}
+
+
+customElements.define('main-header', Header);
+customElements.define('main-footer', Footer);
